7bda73e4bc91614358c476390cd523ff5a6d0012
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2024-10-16 15:16:12 +0700

    fixed

--- config-server/src/main/java/co/istad/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/co/istad/configserver/ConfigServerApplication.java
@@ -3,8 +3,9 @@ package co.istad.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.vault.config.VaultAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {VaultAutoConfiguration.class})
 @EnableConfigServer
 public class ConfigServerApplication {
 
